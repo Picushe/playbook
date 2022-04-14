069c978c327a3f1dd3b710a7dcd51ec15978915b
Picushe <agojeda@outlook.com>
2022-04-13 21:08:58 -0500

    Agregar los ejemplos de objetos

---
+++ weekly_mission_2/examples/example1.js
@@ -0,0 +1,4 @@
+console.log("objetos");
+const myObject = {};
+console.log("Ejemplo 1: Crear un objeto vacío");
+console.log(myObject);
---
+++ weekly_mission_2/examples/example2.js
@@ -0,0 +1,6 @@
+const myObject2 = {
+  name: "Angel",
+  age: 35,
+};
+console.log("Ejemplo 2: Crear un objeto con propiedades");
+console.log(myObject2);
